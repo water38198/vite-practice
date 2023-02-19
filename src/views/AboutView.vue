7340ad181100c45bab6080d05f09352a8823db08
water38198 <46148965+water38198@users.noreply.github.com>
2023-02-19 20:32:16 +0800

    git init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Hi!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: {},
+    };
+  },
+  mounted() {
+    const url = import.meta.env.VITE_PATH;
+    console.log(url);
+  },
+};
+</script>
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
