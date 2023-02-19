7423791af1e4a3f327b0932c1370b13c46a0cae1
water38198 <46148965+water38198@users.noreply.github.com>
2023-02-19 20:55:34 +0800

    deploy

--- assets/AboutView-832bdbf1.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as s,c as a,a as t}from"./index-36e87bf0.js";const n={data(){return{data:{}}},mounted(){console.log("https://randomuser.me/api/")}},_={class:"about"},c=t("h1",null,"This is an about page",-1),r=t("p",null,"Hi!I am Alan",-1),u=[c,r];function i(e,l,d,p,h,m){return s(),a("div",_,u)}const b=o(n,[["render",i]]);export{b as default};
